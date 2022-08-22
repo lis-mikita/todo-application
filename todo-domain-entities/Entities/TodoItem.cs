50c2d345122600499ed4e1b9f4ab4a434e63170c
lis-mikita <fox.nn.aa@gmail.com>
2022-08-22 18:14:10 +0200

    Entities have been added to the todo-domain-entities project.

---
+++ todo-domain-entities/Entities/TodoItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace todo_domain_entities.Entities
+{
+    internal class TodoItem
+    {
+        [Required, Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string TodoListId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        public TodoItemStatus Status { get; set; }
+
+        [Obsolete("Property only used for EF-serialization purposes")]
+        [DataType(DataType.DateTime)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public DateTime CreatedDate 
+        {
+            get => CreatedDate.ToUniversalTime();
+            set => CreatedDate = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+
+        //public DateTime UpdatedDate { get; set; }
+
+
+        [DataType(DataType.DateTime)]
+        [Column("DueTo")]
+        public DateTime DuetoDateTime 
+        {
+            get => DuetoDateTime.ToUniversalTime();
+            set => DuetoDateTime = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+    }
+
+    enum TodoItemStatus
+    {
+        Completed,
+        InProgress,
+        NotStarted
+    }
+}
---
+++ todo-domain-entities/Entities/TodoList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace todo_domain_entities.Entities
+{
+    internal class TodoList
+    {
+        [Required, Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public int Description { get; set; }
+
+    }
+}

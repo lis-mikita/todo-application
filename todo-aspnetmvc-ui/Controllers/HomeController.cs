0d471c19a2454841f99877c5d40b69f5ccc486d8
lis-mikita <fox.nn.aa@gmail.com>
2022-08-23 15:01:50 +0200

    The configure Entity Framework Core was implemented in todo-aspnetmvc-ui project.

--- todo-aspnetmvc-ui/Controllers/HomeController.cs
+++ todo-aspnetmvc-ui/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using todo_aspnetmvc_ui.Models;
+using todo_domain_entities.Interfaces;
 
 namespace todo_aspnetmvc_ui.Controllers
 {
+    [Route("{controller}")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITodoRepository _todoRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITodoRepository todoRepository)
         {
             _logger = logger;
+            _todoRepository = todoRepository;
         }
 
         public IActionResult Index()
@@ -27,6 +32,32 @@ namespace todo_aspnetmvc_ui.Controllers
         {
             return View();
         }
+
+        //view all To Do lists at the application's list view
+        //GET /all
+        [HttpGet("/all")]
+        public string GetAllTodoLists()
+        {
+            return "Hello";
+        }
+
+
+        //create new To Do lists
+        //GET&POST /create
+
+
+        //add To Do entries(items) to a To Do list
+        //enter a title, a description, a due date, a creation date to each To Do item
+        //GET&POST /create/list_id=?
+
+        //change To Do items status to: Completed, In Progress, Not Started
+        //enter a title, a description, a due date, a creation date to each To Do item
+        //GET&PUT /change/list_id=?&item_id=?
+
+
+        //modify a To Do list
+        //GET&PUT /change/list_id=?
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- todo-aspnetmvc-ui/Startup.cs
+++ todo-aspnetmvc-ui/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using todo_domain_entities.EF;
 
 namespace todo_aspnetmvc_ui
 {
@@ -23,7 +25,13 @@ namespace todo_aspnetmvc_ui
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddControllersWithViews();
+
+            services.AddDbContext<TodoDBContext>(opts => {
+                opts.UseSqlServer(
+                    Configuration["ConnectionStrings:TodoConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
